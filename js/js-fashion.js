5097bd328a2872cef9ce7219304ef8e2cfc5fd55
alexandraa25 <ianculescualexandra2@gmail.com>
2024-07-18 18:49:00 +0300

    Add files via upload

---
+++ js/js-fashion.js
@@ -0,0 +1,19 @@
+// Initialize the map
+    function initMap() {
+        var nyc = { lat: 40.7128, lng: -74.0060 };
+        var map = new google.maps.Map(document.getElementById('footer-map'), {
+            zoom: 10,
+            center: nyc
+        });
+        var marker = new google.maps.Marker({
+            position: nyc,
+            map: map
+        });
+    }
+
+    // Ensure the map initializes when the script is loaded
+    document.addEventListener('DOMContentLoaded', function() {
+        if (typeof google !== 'undefined') {
+            initMap();
+        }
+    });
